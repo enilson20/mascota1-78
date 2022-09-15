637f4c9272df3c9c003ac128dd73a9a8d6060ac3
enilson20 <enilson20@hotmail.com>
2022-09-15 03:47:23 -0500

    primer commit

---
+++ MVC-PHP/controller/inicio.php
@@ -0,0 +1,46 @@
+<?php
+require_once("../db/connection.php");
+session_start();
+if($_POST["inicio"]){
+	// inicia sesion para los usuarios
+	$usuario = $_POST["usuario"];
+	$clave = $_POST["clave"];
+	
+	
+	/// consultamos el usuario segun el usuario y la clave
+	$con="select * from usuario where identificacion = '$usuario' and password = '$clave'"; 	
+	$query=mysqli_query($mysqli, $con);
+	$fila=mysqli_fetch_assoc($query);
+	
+	if($fila){		
+		/// si el usario y la clave son correctas, creamo las sessiones 
+			
+		$_SESSION['id_usuario'] = $fila['id_usuario']; 
+		$_SESSION['nombre'] = $fila['nombre']; 
+		$_SESSION['tipo'] = $fila['id_tipo_usuario'];
+		$_SESSION['usuario'] = $fila['identificacion'];
+		
+				/// dependiendo del tipo de usuario lo redireccinamos a una pagina
+		/// si es un client
+		if($_SESSION['tipo'] == 1){
+			header("Location: ../model/admin/index1.php"); 
+			exit();
+		}
+		/// si es un vendedor
+		elseif($_SESSION['tipo'] == 2){
+			header("Location: ../model/funcionario/index1.php"); 
+			exit();		
+		}
+		elseif($_SESSION['tipo'] == 5){
+			header("Location: ../model/propietario_mascota/index1.php"); 
+			exit();		
+		}
+		
+	}else{
+		/// si el usuario y la clave son incorrectas lo lleva a la pagina de inio y se muestra un mensaje
+		header("Location: ../errorlog.html"); 
+		exit();
+	}
+	
+}	
+?>
